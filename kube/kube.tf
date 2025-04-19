78dee3c1d36822ee02a53adb6146224e9e800770
Ben Barkhouse <bfbarkhouse@live.com>
2025-04-19 11:22:17 -0400

    fixed ingress output

--- kube/kube.tf
+++ kube/kube.tf
@@ -82,7 +82,7 @@ data "kubernetes_service" "argocd_server" {
 }
 
 output "argocd_url" {
-  value = data.kubernetes_service.argocd_server.status.0.load_balancer.0.ingress.0.hostname
+  value = data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname
 }
 
 
